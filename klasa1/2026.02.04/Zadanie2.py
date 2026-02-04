c17acb67535d9332bde6e0f09018cb753990bb75
PantoYT <halasawojciech@gmail.com>
2026-02-04 17:50:42 +0100

    update

--- klasa1/2026.02.04/Zadanie2.py
+++ klasa1/2026.02.04/Zadanie2.py
@@ -1,10 +1,25 @@
-def szlaczek():
-    print(f"\n{"*" * 20}\n")
-def znaczekszlaczek(znak):
-    print(f"\n{znak * 20}\n")
-def znaszczekszlaczekileznaczek(znak,ile):
-    print(f"\n{znak * ile}\n")
+def min2(a,b):
+    if a < b: return a
+    else: return b   
 
-szlaczek()
-znaczekszlaczek("@")
-znaszczekszlaczekileznaczek("$",83)
+def max2(a,b):
+    if a > b: return a
+    else: return b
+
+def min3(a,b,c):
+    if a < b:
+        if a < c: return a
+        else: return c
+    else: 
+        if b < c: return b
+        else: return c
+
+def max3(a,b,c):
+    if a > b:
+        if a > c: return a
+        else: return c
+    else: 
+        if b > c: return b
+        else: return c
+
+# for larger sets convert to a list, get min and max that way, don't hard-code
